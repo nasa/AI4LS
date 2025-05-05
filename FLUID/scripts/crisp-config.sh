6ecb0b4c43d1f517162eaf05c8b6f49b55616d42
jcasalet <james.casaletto@gmail.com>
2025-05-05 08:00:09 -0700

    adding support for all local containers

--- FLUID/scripts/crisp-config.sh
+++ FLUID/scripts/crisp-config.sh
@@ -3,20 +3,20 @@ export AGGS="agg-earth agg-iss"
 export COLAB_SHIM="colab-shim"
 
 #export DATA_PATH=/data/work/fluid/project/release/data
-#export DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/FLUID/data
+export DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/AI4LS/FLUID/data
 #export SCRIPT_PATH=/data/work/fluid/project/release/scripts
-export SCRIPT_PATH=/Users/jcasalet/Desktop/CODES/NASA/FLUID/scripts
+export SCRIPT_PATH=/Users/jcasalet/Desktop/CODES/NASA/AI4LS/FLUID/scripts
 
 #export AGG_ISS_DATA_PATH=/home/fluid/data/AGG_ISS
-export AGG_ISS_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/FLUID/data/WORKSPACE/AGG_ISS
+export AGG_ISS_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/AI4LS/FLUID/data/WORKSPACE/AGG_ISS/
 #export AGG_EARTH_DATA_PATH=/home/fluid/data/AGG_EARTH
-export AGG_EARTH_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/FLUID/data/WORKSPACE/AGG_EARTH
+export AGG_EARTH_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/AI4LS/FLUID/data/WORKSPACE/AGG_EARTH
 #export COLAB_ISS_DATA_PATH=/home/fluid/data/COLAB_ISS
-export COLAB_ISS_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/FLUID/data/WORKSPACE/COLAB_ISS
+export COLAB_ISS_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/AI4LS/FLUID/data/WORKSPACE/COLAB_ISS
 #export COLAB_EARTH_DATA_PATH=/home/fluid/data/COLAB_EARTH
-export COLAB_EARTH_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/FLUID/data/WORKSPACE/COLAB_EARTH
+export COLAB_EARTH_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/AI4LS/FLUID/data/WORKSPACE/COLAB_EARTH
 #export COLAB_SHIM_DATA_PATH=/home/fluid/data/COLAB_SHIM
-export COLAB_SHIM_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/FLUID/data/WORKSPACE/COLAB_SHIM
+export COLAB_SHIM_DATA_PATH=/Users/jcasalet/Desktop/CODES/NASA/AI4LS/FLUID/data/WORKSPACE/COLAB_SHIM
 
 #export SCRIPT_PATH=/home/fluid/EBC/AGG/scripts
 
@@ -30,9 +30,9 @@ export WORKSPACE_EARTH_AGG_DIR=/data/WORKSPACE
 export WORKSPACE_EARTH_COLAB_SHIM_DIR=/data/WORKSPACE
 export WORKSPACE_EARTH_COLAB_REAL_DIR=/data/WORKSPACE
 
-export ROUNDS=2
+export ROUNDS=1
 
-export EPOCHS=3
+export EPOCHS=1
 
 export AGG_EARTH_HOST=agg-earth
 export AGG_ISS_HOST=agg-iss
