07fe208c1088bfadfd18162f0acc90318eba92c4
jcasalet <james.casaletto@gmail.com>
2025-05-13 23:32:01 -0700

    sbc4 scripts and dockerfile-arm

--- FLUID/SBC4/scripts/mysync.sh
+++ FLUID/SBC4/scripts/mysync.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+
+
 while true
 do
-	cp ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/request_path/* ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/request_path
-	cp ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/response_path/* ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/response_path
+	rsync -a ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/request_path/ ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/request_path
+	rsync -a ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/response_path/ ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/response_path
 
-	cp ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/request_path/* ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/request_path
-	cp ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/response_path/* ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/response_path
+	rsync -a ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/request_path/ ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/request_path
+	rsync -a ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/response_path/ ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/response_path
 
-	cp ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/request_path/* ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/request_path/
-	cp ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/response_path/* ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/response_path/
+	rsync -a ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/request_path/ ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/request_path/
+	rsync -a ../../data/WORKSPACE/COLAB_SHIM_ISS/WORKSPACE/workspace/response_path/ ../../data/WORKSPACE/AGG_ISS/WORKSPACE/workspace/response_path/
 
-	cp ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/request_path/* ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/request_path/
-	cp ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/response_path/* ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/response_path/
+	rsync -a ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/request_path/ ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/request_path/
+	rsync -a ../../data/WORKSPACE/COLAB_SHIM_MOON/WORKSPACE/workspace/response_path/ ../../data/WORKSPACE/AGG_MOON/WORKSPACE/workspace/response_path/
 
 
 	sleep 5
